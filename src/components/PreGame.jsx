fba08d75ec54dd762ec0cbe3de06ca5abed7b47b
amani <bataganiramani@gmail.com>
2025-05-15 11:50:11 +0200

    pregame screen done

--- src/components/PreGame.jsx
+++ src/components/PreGame.jsx
@@ -1,9 +1,22 @@
 import './PreGame.css'
+import HandIcon from '../assets/images/icon-paper.svg'
+import Scissor from '../assets/images/icon-scissors.svg'
+import Rock from '../assets/images/icon-rock.svg'
 
 export default function PreGame() {
     return (
         <>
-            <div className='triangle'></div>
+            <div className='triangle'>
+                <div className='hand'>
+                    <img src={HandIcon} alt="" />
+                </div>
+                 <div className='scissor'>
+                    <img src={Scissor} alt="" />
+                </div>
+                 <div className='rock'>
+                    <img src={Rock} alt="" />
+                </div>
+            </div>
         </>
     )
 }
