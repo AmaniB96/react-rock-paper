94ee84b8c4640fc3a4bb2e212ad44257b7d80548
amani <bataganiramani@gmail.com>
2025-05-16 09:43:01 +0200

    desing improvement

--- src/components/InGame.jsx
+++ src/components/InGame.jsx
@@ -4,14 +4,14 @@ import './InGame.css'
 export default function InGame({playerChoice, computerChoice}) {
     return (
         <div className='game'>
-            <div>
-                <p>You picked</p>
+            <div className='player'>
+                <p>YOU PICKED</p>
                 <div className={playerChoice.className}>
                 <img src={playerChoice.image} alt="" />
             </div>
             </div>
-            <div>
-                <p>The house picked</p>
+            <div className='player'>
+                <p>THE HOUSE PICKED</p>
                 <div className={computerChoice.className}>
                 <img src={computerChoice.image} alt="" />
             </div>
